6ea198e07acf9e7ea45d020ba4e9202b3041b05a
Alex Denford <pantherNZ@users.noreply.github.com>
2022-10-25 19:05:26 -0700

    Event dispatcher v2

---
+++ EventDispatcherV2.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class EventDispatcherV2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+#if PHYSICS_2D_PACKAGE
+    public UnityEvent<Collider2D> OnTriggerEnter2DEvent;
+    public UnityEvent<Collider2D> OnTriggerExit2DEvent;
+
+    public UnityEvent<Collision2D> OnCollisionEnter2DEvent;
+    public UnityEvent<Collision2D> OnCollisionExit2DEvent;
+#endif
+
+#if PHYSICS_PACKAGE
+    public UnityEvent<Collider> OnTriggerEnterEvent;
+    public UnityEvent<Collider> OnTriggerExitEvent;
+
+    public UnityEvent<Collision> OnCollisionEnterEvent;
+    public UnityEvent<Collision> OnCollisionExitEvent;
+#endif
+
+    public UnityNullable<PointerEventData.InputButton> buttonRestriction;
+    public UnityEvent<PointerEventData> OnPointerEnterEvent;
+    public UnityEvent<PointerEventData> OnPointerExitEvent;
+    public UnityEvent<PointerEventData> OnPointerDownEvent;
+    public UnityEvent<PointerEventData> OnPointerUpEvent;
+
+    private float doubleClickTimer = 0.0f;
+    public float doubleClickInterval = 0.5f;
+    public bool doubleClickUsePointerUp = false;
+    public UnityEvent<PointerEventData> OnDoubleClickEvent;
+
+    public UnityEvent<PointerEventData> OnBeginDragEvent;
+    public UnityEvent<PointerEventData> OnDragEvent;
+    public UnityEvent<PointerEventData> OnEndDragEvent;
+
+    private bool Filter( PointerEventData eventData )
+    {
+        return !buttonRestriction.HasValue || eventData.button == buttonRestriction.Value;
+    }
+
+#if PHYSICS_2D_PACKAGE
+    private bool Filter( Collider2D collision )
+    {
+        return true;
+    }
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        if( Filter( collision ) )
+            OnTriggerEnter2DEvent?.Invoke( collision );
+    }
+
+    private void OnTriggerExit2D( Collider2D collision )
+    {
+        if( Filter( collision ) )
+            OnTriggerExit2DEvent?.Invoke( collision );
+    }
+
+    private void OnCollisionEnter2D( Collision2D collision )
+    {
+        if( Filter( collision ) )
+            OnCollisionEnter2DEvent?.Invoke( collision );
+    }
+
+    private void OnCollisionExit2D( Collision2D collision )
+    {
+        if( Filter( collision ) )
+            OnCollisionExit2DEvent?.Invoke( collision );
+    }
+#endif
+
+#if PHYSICS_PACKAGE
+    private bool Filter( Collider collision )
+    {
+        return true;
+    }
+
+    private void OnTriggerEnter( Collider collision )
+    {
+        if( Filter( collision ) )
+            OnTriggerEnterEvent?.Invoke( collision );
+    }
+
+    private void OnTriggerExit( Collider collision )
+    {
+        if( Filter( collision ) )
+            OnTriggerExitEvent?.Invoke( collision );
+    }
+
+    private void OnCollisionEnter( Collision collision )
+    {
+        if( Filter( collision ) )
+            OnCollisionEnterEvent?.Invoke( collision );
+    }
+
+    private void OnCollisionExit( Collision collision )
+    {
+        if( Filter( collision ) )
+            OnCollisionExitEvent?.Invoke( collision );
+    }
+#endif
+
+    void IPointerEnterHandler.OnPointerEnter( PointerEventData eventData )
+    {
+        if( Filter( eventData ) )
+            OnPointerEnterEvent?.Invoke( eventData );
+    }
+
+    void IPointerExitHandler.OnPointerExit( PointerEventData eventData )
+    {
+        if( Filter( eventData ) ) 
+            OnPointerExitEvent?.Invoke( eventData );
+    }
+
+    void IPointerDownHandler.OnPointerDown( PointerEventData eventData )
+    {
+        if( Filter( eventData ) )
+        {
+            OnPointerDownEvent?.Invoke( eventData );
+
+            if( !doubleClickUsePointerUp )
+            {
+                if( Time.time - doubleClickTimer <= doubleClickInterval )
+                    OnDoubleClickEvent?.Invoke( eventData );
+                doubleClickTimer = Time.time;
+            }
+        }
+    }
+
+    void IPointerUpHandler.OnPointerUp( PointerEventData eventData )
+    {
+        if( Filter( eventData ) )
+        {
+            OnPointerUpEvent?.Invoke( eventData );
+
+            if( doubleClickUsePointerUp )
+            {
+                if( Time.time - doubleClickTimer <= doubleClickInterval )
+                    OnDoubleClickEvent?.Invoke( eventData );
+                doubleClickTimer = Time.time;
+            }
+        }
+    }
+
+    void IBeginDragHandler.OnBeginDrag( PointerEventData eventData )
+    {
+        if( Filter( eventData ) ) 
+            OnBeginDragEvent?.Invoke( eventData );
+    }
+
+    void IEndDragHandler.OnEndDrag( PointerEventData eventData )
+    {
+        if( Filter( eventData ) ) 
+            OnEndDragEvent?.Invoke( eventData );
+    }
+
+    void IDragHandler.OnDrag( PointerEventData eventData )
+    {
+        if( Filter( eventData ) ) 
+            OnDragEvent?.Invoke( eventData );
+    }
+}
---
+++ UnityNullable.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// Serializable Nullable (UnityNullable) Does the same as C# System.Nullable, except it's an ordinary
+/// serializable struct, allowing unity to serialize it and show it in the inspector.
+/// </summary>
+[System.Serializable]
+public struct UnityNullable<T> where T : struct
+{
+    public T Value
+    {
+        get
+        {
+            if( !HasValue )
+                throw new System.InvalidOperationException( "Serializable nullable object must have a value." );
+            return v;
+        }
+    }
+
+    public bool HasValue { get { return hasValue; } }
+
+    [SerializeField]
+    private T v;
+
+    [SerializeField]
+    private bool hasValue;
+
+    public UnityNullable( bool hasValue, T v )
+    {
+        this.v = v;
+        this.hasValue = hasValue;
+    }
+
+    private UnityNullable( T v )
+    {
+        this.v = v;
+        this.hasValue = true;
+    }
+
+    public static implicit operator UnityNullable<T>( T value )
+    {
+        return new UnityNullable<T>( value );
+    }
+
+    public static implicit operator UnityNullable<T>( System.Nullable<T> value )
+    {
+        return value.HasValue ? new UnityNullable<T>( value.Value ) : new UnityNullable<T>();
+    }
+
+    public static implicit operator System.Nullable<T>( UnityNullable<T> value )
+    {
+        return value.HasValue ? ( T? )value.Value : null;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer( typeof( UnityNullable<> ) )]
+internal class UnityNullableDrawer : PropertyDrawer
+{
+    public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+    {
+        EditorGUI.BeginProperty( position, label, property );
+
+        // Draw label
+        position = EditorGUI.PrefixLabel( position, GUIUtility.GetControlID( FocusType.Passive ), label );
+
+        // Don't make child fields be indented
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // Calculate rects
+        var setRect = new Rect( position.x, position.y, 15, position.height );
+        var consumed = setRect.width + 5;
+        var valueRect = new Rect( position.x + consumed, position.y, position.width - consumed, position.height );
+
+        // Draw fields - pass GUIContent.none to each so they are drawn without labels
+        var hasValueProp = property.FindPropertyRelative( "hasValue" );
+        EditorGUI.PropertyField( setRect, hasValueProp, GUIContent.none );
+        bool guiEnabled = GUI.enabled;
+        GUI.enabled = guiEnabled && hasValueProp.boolValue;
+        EditorGUI.PropertyField( valueRect, property.FindPropertyRelative( "v" ), GUIContent.none );
+        GUI.enabled = guiEnabled;
+
+        // Set indent back to what it was
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
