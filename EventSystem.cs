f00df994be540bbfec28c106c5cfef83d0228a74
Alex Denford <pantherNZ@users.noreply.github.com>
2022-12-02 01:40:59 -0800

    Added QueueEvent to EventSystem

--- EventSystem.cs
+++ EventSystem.cs
@@ -36,6 +36,8 @@ public class EventSystem
     }
 
     private readonly List<IEventReceiver> receivers = new List<IEventReceiver>();
+    private readonly Queue<Pair<IBaseEvent, IEventReceiver>> queuedEvents = new Queue<Pair<IBaseEvent, IEventReceiver>>();
+    private int iterationDepth = 0;
 
     public void AddSubscriber( IEventReceiver receiver )
     {
@@ -50,14 +52,46 @@ public class EventSystem
 
     public void TriggerEvent( IBaseEvent e )
     {
-        foreach( var receiver in receivers )
-            receiver.OnEventReceived( e );
+        TriggerEvent( e, null );
     }
 
-    public void TriggerEvent( IBaseEvent e, IEventReceiver caller )
+    public void TriggerEvent( IBaseEvent e, IEventReceiver callerToIgnore )
     {
+        iterationDepth++;
+
         foreach( var receiver in receivers )
-            if( receiver != caller )
+            if( receiver != null && receiver != callerToIgnore )
                 receiver.OnEventReceived( e );
+
+        iterationDepth--;
+
+        if( iterationDepth == 0 && queuedEvents.Count > 0 )
+        {
+            int safety = 0;
+            while( queuedEvents.Count > 0 && safety++ <= 100 )
+            {
+                var queuedEvent = queuedEvents.Dequeue();
+                TriggerEvent( queuedEvent.First, queuedEvent.Second );
+            }
+
+            if( safety >= 100 )
+                Debug.LogError( "EventSystem hit recursion limit with queue events" );
+
+            queuedEvents.Clear();
+        }
+    }
+
+    // Waits until all current events are done processing before calling this one (will be instant if not currently iterating an event callback)
+    public void QueueEvent( IBaseEvent e )
+    {
+        QueueEvent( e, null );
+    }
+
+    public void QueueEvent( IBaseEvent e, IEventReceiver callerToIgnore )
+    {
+        if( iterationDepth == 0 )
+            TriggerEvent( e, callerToIgnore );
+        else
+            queuedEvents.Enqueue( new Pair<IBaseEvent, IEventReceiver>( e, callerToIgnore ) );
     }
 }
