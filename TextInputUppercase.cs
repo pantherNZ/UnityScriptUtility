b978522d3989fdf97ba74207f27ef1a077d98950
Alex Denford <pantherNZ@users.noreply.github.com>
2023-08-28 23:30:09 -0700

    Tmpro input util

---
+++ TextInputUppercase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TMPro.TMP_InputField))]
+public class TextInputUppercase : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<TMPro.TMP_InputField>().onValidateInput += delegate ( string input, int charIndex, char addedChar )
+        {
+            return char.ToUpper( addedChar );
+        };
+    }
+}
