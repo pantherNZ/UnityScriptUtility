ec118e11a1993bdf3d11ab5e4c8c38dbe177beda
Alex Denford <pantherNZ@users.noreply.github.com>
2024-06-03 01:30:57 -0700

    Texture2d creator helper

---
+++ Editor/TextureArrayWizard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+partial class Utility
+{
+	public static Texture2DArray CreateTexure2DArray( params Texture2D[] tex )
+	{
+		bool mips = tex[0].mipmapCount > 1;
+
+		var texArray = new Texture2DArray( tex[0].width, tex[0].height, tex.Length, tex[0].format, false, false )
+		{
+			anisoLevel = tex[0].anisoLevel,
+			filterMode = tex[0].filterMode,
+			wrapMode = tex[0].wrapMode
+		};
+
+		// Go over all the textures and add to array
+		for ( int texIndex = 0; texIndex < tex.Length; texIndex++ )
+		{
+			if ( tex[texIndex] != null )
+			{
+				for ( int mip = 0; mip < 1/*tex[texIndex].mipmapCount*/; mip++ )
+					Graphics.CopyTexture( tex[texIndex], 0, mip, texArray, texIndex, mip );
+			}
+		}
+
+		return texArray;
+	}
+}
+
+public class TextureArrayWizard : ScriptableWizard
+{
+	[MenuItem( "Assets/Create/Texture Array" )]
+	static void CreateWizard()
+	{
+		DisplayWizard<TextureArrayWizard>("Create Texture Array", "Create");
+	}
+
+	void OnWizardCreate()
+	{
+		if ( textures.Length == 0 )
+		{
+			return;
+		}
+
+		string path = EditorUtility.SaveFilePanelInProject("Save Texture Array", "Texture Array", "asset", "Save Texture Array");
+
+		if ( path.Length == 0 )
+		{
+			return;
+		}
+
+		var texArray = Utility.CreateTexure2DArray( textures );
+		AssetDatabase.CreateAsset( texArray, path );
+	}
+
+	public Texture2D[] textures;
+}
