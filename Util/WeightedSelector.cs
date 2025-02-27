388f957879c69ad62924ab822620f16795ba375f
Alex Denford <pantherNZ@users.noreply.github.com>
2025-02-27 00:42:42 -0800

    Update WeightedSelector.cs

--- Util/WeightedSelector.cs
+++ Util/WeightedSelector.cs
@@ -1,38 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class WeightedSelector<T>
 {
-    public WeightedSelector( Func<int, int, int> randomGenerator )
-    {
-        randomGeneratorPred = randomGenerator;
-    }
+	public WeightedSelector( Func<int, int> randomGenerator )
+	{
+		randomGeneratorPred = randomGenerator;
+	}
 
-    public WeightedSelector( Utility.IRandom rng = null )
-    {
-        randomGeneratorPred = ( int min, int max ) => { return ( rng ?? Utility.DefaultRng ).Range( min, max ); };
-    }
+	public WeightedSelector( Utility.IRandom rng = null )
+	{
+		this.rng = rng;
+	}
 
-    public void AddItem( T item, int weight )
-    {
-        if( weight <= 0 )
-            return;
-        total += weight;
+	public void AddItem( T item, int weight )
+	{
+		if ( weight > 0 )
+		{
+			total += weight;
+			items.Add( ( item, total ) );
+		}
+	}
 
-        if( randomGeneratorPred( 0, total - 1 ) < weight )
-            current = item;
-    }
+	private int GetRandom()
+	{
+		if ( items.Count <= 1 )
+			return 0;
 
-    public T GetResult()
-    {
-        return current;
-    }
+		if ( rng != null )
+			return rng.Range( 0, total ) + 1;
 
-    public bool HasResult()
-    {
-        return total != 0;
-    }
+		if ( randomGeneratorPred != null )
+			return randomGeneratorPred( total ) + 1;
 
-    private T current;
-    private int total = 0;
-    private Func<int, int, int> randomGeneratorPred;
+		return Utility.DefaultRng.Range( 0, total ) + 1;
+	}
+
+	public T GetResult()
+	{
+		if ( !HasResult() )
+			return default;
+
+		if ( items.Count == 1 )
+			return items[0].Item1;
+
+		var randomVal = GetRandom();
+		var resultIdx = items.BinarySearch( (default( T ), randomVal),
+			Comparer<(T, int)>.Create( ( x, y ) => x.Item2 - y.Item2 ) );
+
+		if ( resultIdx < 0 )
+		{
+			resultIdx = ~resultIdx;
+			if ( resultIdx > items.Count )
+				return default;
+		}
+
+		return items[resultIdx].Item1;
+	}
+
+	public bool HasResult()
+	{
+		return items.Count > 0;
+	}
+
+	private readonly List<(T, int)> items = new();
+	private int total = 0;
+	private readonly Func<int, int> randomGeneratorPred;
+	private readonly Utility.IRandom rng;
 }
