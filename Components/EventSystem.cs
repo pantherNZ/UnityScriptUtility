0cc161bc30aea309520842ce33c9e6730604733c
Alex Denford <pantherNZ@users.noreply.github.com>
2025-02-27 00:41:08 -0800

    New event system tech

--- Components/EventSystem.cs
+++ Components/EventSystem.cs
@@ -3,57 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public abstract class IBaseEvent { }
-
-public interface IEventReceiver
-{
-    public abstract void OnEventReceived( IBaseEvent e );
+public interface IEventReceiver 
+{ 
+	bool receivesEvents { get; set; }
+	void AddSubscription( IEventSystem system );
+	void RemoveSubscription( IEventSystem system );
 }
 
-public abstract class EventReceiverInstance : MonoBehaviour, IEventReceiver
+public class EventReceiver : IEventReceiver
 {
-    protected virtual void Start()
-    {
-        EventSystem.Instance.AddSubscriber( this );
-    }
-
-    protected virtual void OnEnable()
-    {
-        if( modifyListenerWithEnableDisable )
-            EventSystem.Instance.AddSubscriber( this );
-    }
-
-    protected virtual void OnDisable()
-    {
-        if( modifyListenerWithEnableDisable )
-            EventSystem.Instance.RemoveSubscriber( this );
-    }
-
-    public abstract void OnEventReceived( IBaseEvent e );
-
-    public bool modifyListenerWithEnableDisable = true;
-
-	// TODO
-	//public bool enableDebugLogging = false;
+	bool IEventReceiver.receivesEvents { get => _receivesEvents; set => _receivesEvents = value; }
+	private bool _receivesEvents;
+	private List<IEventSystem> subscribedEvents = new();
+	public void AddSubscription( IEventSystem system ) => subscribedEvents.Add( system );
+	public void RemoveSubscription( IEventSystem system ) => subscribedEvents.Remove( system ); 
+	public void UnsubscribeFromAll()
+	{
+		while ( !subscribedEvents.IsEmpty() )
+			subscribedEvents.Last().RemoveSubscriber( this, true );
+		subscribedEvents.Clear();
+	}
+	public EventReceiver()
+	{
+		_receivesEvents = true;
+	}
+	~EventReceiver()
+	{
+		UnsubscribeFromAll();
+	}
 }
 
-public class EventSystem
+public class MonoEventReceiver : MonoBehaviour, IEventReceiver
 {
-    static EventSystem _Instance;
-	public static EventSystem Instance
-    {
-        get
-        {
-            if( _Instance == null )
-                _Instance = new EventSystem();
-           return _Instance;
-        }
+	public bool receivesEvents { get => receivesEventsOverride ?? isActiveAndEnabled; set => receivesEventsOverride = value; }
+	public bool? receivesEventsOverride;
+	private List<IEventSystem> subscribedEvents = new();
+	public void AddSubscription( IEventSystem system ) => subscribedEvents.Add( system );
+	public void RemoveSubscription( IEventSystem system ) => subscribedEvents.Remove( system );
+	public void UnsubscribeFromAll()
+	{
+		while( !subscribedEvents.IsEmpty() )
+			subscribedEvents.Last().RemoveSubscriber( this, true );
+		subscribedEvents.Clear();
+	}
+	protected virtual void OnDestroy()
+	{
+		UnsubscribeFromAll();
+	}
+}
 
-        private set { }
-    }
+public interface IEvent { }
+public abstract class BaseEvent<EventType> : IEvent
+{
+	public static void Subscribe( IEventReceiver instance, Action<EventType> callback ) 
+		=> Instance.AddSubscriber( instance, callback );
+	public static void Unsubscribe( IEventReceiver receiver ) 
+		=> Instance.RemoveSubscriber( receiver );
+	public static void Trigger( EventType eventData, IEventReceiver callerToIgnore = null )
+		=> Instance.TriggerEvent( eventData, callerToIgnore );
+	public static void Queue( EventType eventData, IEventReceiver callerToIgnore = null )
+		=> Instance.QueueEvent( eventData, callerToIgnore );
+
+	static EventSystem<EventType> _Instance;
+	private static EventSystem<EventType> Instance
+	{
+		get
+		{
+			if ( _Instance == null )
+				_Instance = new EventSystem<EventType>();
+			return _Instance;
+		}
+	}
+}
+
+public interface IEventSystem 
+{
+	void RemoveSubscriber( IEventReceiver receiver, bool force = false );
+
 	public static bool EnableLogging = true;
+}
 
-    private enum QueuedSubscriber
+public class EventSystem<EventType> : IEventSystem
+{
+	private enum QueuedSubscriber
     {
         Add,
         Remove,
@@ -61,75 +93,76 @@ public class EventSystem
 
 	private class EventReceiverData
 	{
-		public IEventReceiver receiver;
-		public Type[] eventTypes;
+		public WeakReference<IEventReceiver> receiver;
+		public Action<EventType> callback;
 	}
 
     private readonly List<EventReceiverData> receivers = new List<EventReceiverData>();
-    private readonly Queue<Pair<IBaseEvent, IEventReceiver>> queuedEvents = new Queue<Pair<IBaseEvent, IEventReceiver>>();
-    private readonly Queue<Pair<QueuedSubscriber, EventReceiverData>> queuedSubscribers = new Queue<Pair<QueuedSubscriber, EventReceiverData>>();
+	private readonly Queue<(EventType, IEventReceiver)> queuedEvents = new Queue<(EventType, IEventReceiver)>();
+	private readonly Queue<(QueuedSubscriber, EventReceiverData)> queuedSubscribers = new Queue<(QueuedSubscriber, EventReceiverData)>();
     private int iterationDepth = 0;
 
 	private void Log( string msg )
 	{
-		if ( EnableLogging )
+		if ( IEventSystem.EnableLogging )
 		{
 			Debug.Log( "[EVENT SYSTEM] " + msg );
 		}
 	}
 
-	public void AddSubscriber( IEventReceiver receiver )
+	public void AddSubscriber( IEventReceiver receiver, Action<EventType> callback )
 	{
-		AddSubscriber( receiver, null );
-	}
+		if ( !Application.isPlaying )
+			return;
 
-	public void AddSubscriber( IEventReceiver receiver, params Type[] eventTypes )
-	{
-		var newReceiver = new EventReceiverData 
+		CleanupNullSubscribers();
+
+		var newReceiver = new EventReceiverData
 		{
-			receiver = receiver,
-			eventTypes = eventTypes,
+			receiver = new WeakReference<IEventReceiver>( receiver, false ),
+			callback = callback,
 		};
+
+		receiver.AddSubscription( this );
 
 		if ( iterationDepth == 0 )
 		{
-			if ( !receivers.Any( x => x.receiver == receiver ) )
+			if ( !receivers.Any( x => x.receiver.TryGetTarget( out var targ ) && targ == receiver ) )
 			{
 				receivers.Add( newReceiver );
-				var eventTypesInfo = eventTypes != null ? $", event types: {string.Join( ", ", eventTypes.Select( x => x.ToString() ) )}" : string.Empty;
-				Log( $"Subscriber added: {receiver}${eventTypesInfo}" );
+				Log( $"Subscriber added: {receiver}" );
 			}
 		}
 		else
 		{
-			queuedSubscribers.Enqueue( new Pair<QueuedSubscriber, EventReceiverData>( QueuedSubscriber.Add, newReceiver ) );
+			queuedSubscribers.Enqueue( ( QueuedSubscriber.Add, newReceiver ) );
 			Log( "Subscriber queued for add: " + receiver.ToString() );
 		}
 	}
 
-	public void RemoveSubscriber( IEventReceiver receiver )
+	public void RemoveSubscriber( IEventReceiver receiver, bool force = false )
     {
-        if( iterationDepth == 0 )
+		CleanupNullSubscribers();
+
+		if ( force || iterationDepth == 0 )
         {
-            receivers.Remove( x => x.receiver == receiver );
+            receivers.Remove( x => !x.receiver.TryGetTarget( out var targ ) || targ == receiver );
+			receiver.RemoveSubscription( this );
 			Log( "Subscriber removed: " + receiver.ToString() );
 		}
         else
         {
-			var removeReceiver = new EventReceiverData { receiver = receiver };
-			queuedSubscribers.Enqueue( new Pair<QueuedSubscriber, EventReceiverData>( QueuedSubscriber.Remove, removeReceiver ) );
+			var removeReceiver = new EventReceiverData { receiver = new WeakReference<IEventReceiver>( receiver ) };
+			queuedSubscribers.Enqueue( ( QueuedSubscriber.Remove, removeReceiver ) );
 			Log( "Subscriber queued for remove: " + receiver.ToString() );
 		}
     }
 
-    public void TriggerEvent( IBaseEvent e )
-    {
-        TriggerEvent( e, null );
-    }
-
-    public void TriggerEvent( IBaseEvent e, IEventReceiver callerToIgnore )
+    public void TriggerEvent( EventType e, IEventReceiver callerToIgnore = null )
     {
 		Log( "Event triggered start: " + e.ToString() );
+
+		CleanupNullSubscribers();
 
 		iterationDepth++;
 
@@ -137,10 +170,14 @@ public class EventSystem
 		var receiversCalled = 0; 
         foreach( var receiverData in receivers )
         {
-            if( receiverData != null && receiverData != callerToIgnore &&
-				( receiverData.eventTypes == null || receiverData.eventTypes.IsEmpty() || receiverData.eventTypes.Contains( e.GetType() ) ) )
+            if( receiverData != null && 
+				receiverData.receiver != null &&
+				receiverData.receiver.TryGetTarget( out var receiver ) &&
+				receiver != null &&
+				receiver != callerToIgnore &&
+				receiver.receivesEvents )
             {
-				receiverData.receiver.OnEventReceived( e );
+				receiverData.callback( e );
 				++receiversCalled;
 				Debug.Assert( numRecievers == receivers.Count );
             }
@@ -158,7 +195,7 @@ public class EventSystem
                 while( queuedEvents.Count > 0 && safety++ <= 100 )
                 {
                     var queuedEvent = queuedEvents.Dequeue();
-                    TriggerEvent( queuedEvent.First, queuedEvent.Second );
+                    TriggerEvent( queuedEvent.Item1, queuedEvent.Item2 );
                 }
 
                 if( safety >= 100 )
@@ -171,10 +208,13 @@ public class EventSystem
             {
                 foreach( var queued in queuedSubscribers )
                 {
-                    if( queued.First == QueuedSubscriber.Add )
-                        AddSubscriber( queued.Second.receiver, queued.Second.eventTypes );
-                    else
-                        RemoveSubscriber( queued.Second.receiver );
+					if ( queued.Item2.receiver.TryGetTarget( out var receiver ) )
+					{
+						if ( queued.Item1 == QueuedSubscriber.Add )
+							AddSubscriber( receiver, queued.Item2.callback );
+						else
+							RemoveSubscriber( receiver );
+					}
                 }
 
                 queuedSubscribers.Clear();
@@ -182,13 +222,29 @@ public class EventSystem
         }
     }
 
+	private void CleanupNullSubscribers()
+	{
+		var toRemove = receivers.Where( x => x == null ||
+			x.receiver == null || 
+			x.callback == null || 
+			!x.receiver.TryGetTarget( out var receiver ) ||
+			receiver == null ).ToList();
+
+		foreach ( var remove in toRemove )
+		{
+			if( remove.receiver.TryGetTarget( out var targ ) )
+				targ.RemoveSubscription( this );
+			receivers.Remove( remove );
+		}
+	}
+
     // Waits until all current events are done processing before calling this one (will be instant if not currently iterating an event callback)
-    public void QueueEvent( IBaseEvent e )
+    public void QueueEvent( EventType e )
     {
         QueueEvent( e, null );
     }
 
-    public void QueueEvent( IBaseEvent e, IEventReceiver callerToIgnore )
+    public void QueueEvent( EventType e, IEventReceiver callerToIgnore )
     {
 		if ( iterationDepth == 0 )
 		{
@@ -196,7 +252,7 @@ public class EventSystem
 		}
 		else
 		{
-			queuedEvents.Enqueue( new Pair<IBaseEvent, IEventReceiver>( e, callerToIgnore ) );
+			queuedEvents.Enqueue( ( e, callerToIgnore ) );
 			Log( $"Event queued: {e}" );
 		}
     }
